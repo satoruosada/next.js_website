a27afa1cd76a4a52734411771c8356d365426186
osa-s <satoru.5714@gmail.com>
2026-02-07 13:34:52 +0900

    7章まで完了

---
+++ app/news/search/page.tsx
@@ -0,0 +1,26 @@
+import { getNewsList } from '@/app/_libs/microcms';
+import { NEWS_LIST_LIMIT } from '@/app/_constants';
+import NewsList from '@/app/_components/NewsList';
+import SearchField from '@/app/_components/SearchField';
+
+type Props = {
+    searchParams: Promise<{
+        q?: string;
+    }>;
+};
+
+export default async function Page({ searchParams }: Props) {
+    const { q } = await searchParams;
+
+    const { contents: news } = await getNewsList({
+        limit: NEWS_LIST_LIMIT,
+        q,
+    });
+
+    return (
+        <>
+            <SearchField />
+            <NewsList news={news} />
+        </>
+    );
+}
